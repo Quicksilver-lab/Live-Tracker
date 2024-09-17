e8c165aa748820142763850d50d4d8887a0b0bcb
Quicksilver-lab <hi.sadaqatali@gmail.com>
2024-09-17 22:27:55 +0500

    Completed

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const express = require("express");
-const app = express();
-const path = require("path");
 const http = require("http");
-
 const socketio = require("socket.io");
+const path = require("path");
 
+const app = express();
 const server = http.createServer(app);
-
-const io = require("socket.io");
+const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, publiC)));
 
-io.on("connection", function (socket) {
-  console.log("Connected");
-});
-appp.get("/", function (req, res) {
-  res.rener("index");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-server.listen("3000");
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send-location", (data) => {
+    console.log("Location received:", data);
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("update-text", (text) => {
+    console.log("Text/Emoji received:", text);
+    io.emit("update-text", { id: socket.id, text });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,95 @@
 const socket = io();
+let map;
+const markers = {};
 
-if (navigator.geolocation) {
-}
+document.addEventListener("DOMContentLoaded", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log("User's location:", { latitude, longitude });
 
-L.map("map").setView([0, 0], 10);
+        map = L.map("map").setView([latitude, longitude], 16);
 
-L.tyleLayer("htts://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.github.com/Quicksilver-lab">Quicksilver-lab</a> contributors',
+        }).addTo(map);
+
+        L.marker([latitude, longitude])
+          .addTo(map)
+          .bindPopup("You are here")
+          .openPopup();
+
+        setInterval(() => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              console.log("Sending location:", { latitude, longitude });
+              socket.emit("send-location", { lat: latitude, lng: longitude });
+            },
+            (error) => {
+              console.error("Geolocation error:", error.message);
+            },
+            { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+          );
+        }, 5000);
+      },
+      (error) => {
+        console.error("Geolocation error:", error.message);
+        map = L.map("map").setView([0, 0], 16);
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.github.com/Quicksilver-lab">Quicksilver-lab</a> contributors',
+        }).addTo(map);
+      },
+      { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+    map = L.map("map").setView([0, 0], 16);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.github.com/Quicksilver-lab">Quicksilver-lab</a> contributors',
+    }).addTo(map);
+  }
+});
+
+socket.on("receive-location", (data) => {
+  const { id, lat, lng, text } = data;
+  console.log("Received location:", { id, lat, lng });
+
+  if (markers[id]) {
+    markers[id].setLatLng([lat, lng]);
+    markers[id].setPopupContent(text || "");
+  } else {
+    markers[id] = L.marker([lat, lng])
+      .bindPopup(text || "")
+      .addTo(map);
+  }
+});
+
+socket.on("update-text", (data) => {
+  const { id, text } = data;
+  console.log("Received text/emoji:", { id, text });
+
+  if (markers[id]) {
+    markers[id].setPopupContent(text);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
+
+document.getElementById("submit-button").addEventListener("click", () => {
+  const text = document.getElementById("text-input").value;
+  console.log("Sending text/emoji:", text);
+  socket.emit("update-text", { text });
+  document.getElementById("text-input").value = "";
+});
